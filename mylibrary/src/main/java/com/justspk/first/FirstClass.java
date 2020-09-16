cb67ace069688c65149cf1a3c4d746e5c96b144f
Pradeep Kumar S <kumarkumar0058@gmail.com>
2020-09-16 18:10:59 +0530

    Toast Commit

--- mylibrary/src/main/java/com/justspk/first/FirstClass.java
+++ mylibrary/src/main/java/com/justspk/first/FirstClass.java
@@ -1,5 +1,10 @@
 package com.justspk.first;
 
-public class FirstClass {
+import android.content.Context;
+import android.widget.Toast;
 
+public class FirstClass {
+    public static void showToast(Context context,String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 }
